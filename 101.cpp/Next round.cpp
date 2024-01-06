87d46ff62d06424c07f921ca596817ee71a40a3b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-06 19:39:38 +0100

    Update Next round.cpp

--- 101.cpp/Next round.cpp
+++ 101.cpp/Next round.cpp
@@ -17,3 +17,30 @@ int main(){
     }
     cout << count << endl;
 }
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int cnt = 0;
+    int n, k;
+    cin >> n >> k;
+    
+    int arr[n + 2];
+    
+    while (cin >> n >> k)
+    {
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+            
+        int src = arr[k - 1];
+        
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] >= src && arr[i] != 0)
+                cnt++;
+        }
+        cout << cnt;
+    }
+    return 0;
+}
