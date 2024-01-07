ad9faca13bcd2eded084944d278b740d58ae73ca
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-07 09:30:21 +0100

    Update Petya and strings.cpp

--- 101.cpp/Petya and strings.cpp
+++ 101.cpp/Petya and strings.cpp
@@ -48,3 +48,27 @@ int main()
     }
     cout<< 0;
 }
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (a[i] < 92)
+            a[i] += 32;
+        if (b[i] < 92)
+            b[i] += 32;
+    }
+    if (a < b)
+        cout << -1;
+    else if (a > b)
+        cout << 1;
+    else
+        cout << 0;
+        
+    return 0;
+}
