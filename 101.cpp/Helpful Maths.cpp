b64d3f267d1827bc6fd93d56f6b126f164baf833
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-07 17:47:00 +0100

    Update Helpful Maths.cpp

--- 101.cpp/Helpful Maths.cpp
+++ 101.cpp/Helpful Maths.cpp
@@ -17,3 +17,34 @@ int main()
     }
     cout<<s[s.size()-1];
 }
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >>s;
+    int n= (s.size() +1)/2;
+    int arr[n], x=0, y=0, min=0;
+    while( s.size() >x)
+    {
+        arr[y++] = s[x];
+        x =x+2;
+    }
+    for(int i =0; i< n; i++)
+    {
+        min =i;
+        for(int j =i+1; j< n; j++)
+        {
+            if(arr[j] < arr[min])
+                min =j;
+        }
+        swap(arr[min], arr[i]);
+    }
+    x =0, y =0;
+    while(x < s.size())
+    {
+        s[x] =arr[y++];
+        x+=2;
+    }
+    cout << s;
+}
