a4e616024a2fb72af768106589a0735e2b8dbf5c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-07 09:49:40 +0100

    Update Petya and strings.cpp

--- 101.cpp/Petya and strings.cpp
+++ 101.cpp/Petya and strings.cpp
@@ -72,3 +72,25 @@ int main()
         
     return 0;
 }
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+
+    transform(a.begin(), a.end(), a.begin(), ::tolower);
+    transform(b.begin(), b.end(), b.begin(), ::tolower);
+
+    int n = a.compare(b);
+
+    if (n < 0)
+        cout << -1;
+    else if (n > 0)
+        cout << 1;
+    else
+        cout << 0;
+
+    return 0;
+}
