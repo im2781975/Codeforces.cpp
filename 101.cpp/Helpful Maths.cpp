a70670ec12d5e28e602b6dcc213fd20c50f1d7d7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-08 05:16:10 +0100

    Update Helpful Maths.cpp

--- 101.cpp/Helpful Maths.cpp
+++ 101.cpp/Helpful Maths.cpp
@@ -113,3 +113,26 @@ int main()
         cout<<"+" <<ans[i];
     }
 }
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;
+    cin >>s;
+    vector<int>ans;
+    for(int i=0; i<s.size(); i+=2)
+    {
+        int x=s[i] -'0';
+        ans.push_back(x);
+    }
+    sort(ans.begin(), ans.end());
+    for(int i=0; i<ans.size(); i++)
+    {
+        cout<< ans[i];
+        if(i!=ans.size()-1)
+            cout<< "+";
+    }
+}
