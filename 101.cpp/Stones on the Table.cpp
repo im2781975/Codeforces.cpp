ed878288849737e241afcaf69d327d500068a567
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-08 09:25:04 +0100

    Update Stones on the Table.cpp

--- 101.cpp/Stones on the Table.cpp
+++ 101.cpp/Stones on the Table.cpp
@@ -51,3 +51,16 @@ int main()
         }
     }
 }
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int n, cnt=0;
+    cin >>n;
+    string s;
+    cin >>s;
+    for(int i=0; i<n-1; i++)
+        cnt+=s[i]==s[i+1];
+    cout<<cnt;
+}
