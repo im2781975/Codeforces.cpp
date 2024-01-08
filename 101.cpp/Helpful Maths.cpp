d3a83ecb29a936dd7e0f2de4ef845ba18332eaa8
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-08 04:43:02 +0100

    Update Helpful Maths.cpp

--- 101.cpp/Helpful Maths.cpp
+++ 101.cpp/Helpful Maths.cpp
@@ -48,3 +48,49 @@ int main()
     }
     cout << s;
 }
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;
+    cin >>s;
+    int ones =0, twos =0, threes =0;
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i]=='1')
+            ones++;
+        if(s[i] == '2')
+            twos++;
+        if(s[i] == '3')
+            threes++;
+    }
+    if(ones!=0)
+    {
+        cout <<1;
+        for(int i=0; i<ones-1; i++)
+            cout<< '+' << 1;
+        for(int i=0; i<twos; i++)
+            cout<< '+' <<2;
+        for(int i=0; i<threes; i++)
+            cout <<'+' <<3;
+            
+        return 0;
+    }
+    if(twos!=0)
+    {
+        cout <<2;
+        for(int i=0; i<twos-1; i++)
+            cout<<'+' <<2;
+        for(int i=0; i<threes; i++)
+            cout<<'+' <<3;
+        return 0;
+    }
+    if(threes!=0)
+    {
+        cout <<3;
+        for(int i=0; i<threes-1; i++)
+                cout<<'+'<<3;
+        return 0;
+    }
+}
